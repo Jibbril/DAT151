b49088ecaa2a3744f7e8e1409544d181307c4ddd
jibbril.b.n@gmail.com <jibbril.b.n@gmail.com>
2021-11-05 16:12:21 +0100

    Solve test 2 of first lab

---
+++ lab1/Haskell/dummy.cc
@@ -0,0 +1,7 @@
+// a small C++ program
+#include <iostream>
+int main( int arg1)
+{
+  	std::cout << "Hejhej" << std::endl;
+	return 0;
+}
---
+++ lab1/Java/test-1.cc
@@ -0,0 +1,8 @@
+// a small C++ program
+#include <iostream>
+
+int main(bool a, bool b, bool c)
+{
+  	std::cout << "Hello, world!" << std::endl;
+	return 0;
+}
